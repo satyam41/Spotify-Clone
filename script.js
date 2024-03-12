f8b56c8371b44905a74108c1fb44322392e91ee0
Satyam Krishna <thesatyam10@gmail.com>
2024-03-12 16:14:34 +0530

    update the code and add some feature like play the specific song, play and pause the song, display the song name and song duration in the playbar area

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let currentSong = new Audio();
+
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs() {
     let a = await fetch('http://127.0.0.1:3000/songs/');
     let response = await a.text();
@@ -18,23 +34,63 @@ async function getSongs() {
     return songs;
 }
 
+const playMusic = (track) => {
+    // let audio = new Audio("/songs/" + track);
+    currentSong.src = "/songs/" + track;
+    currentSong.play();
+    play.src = "/img/pause.svg";
+    document.querySelector(".songinfo").innerHTML = track;
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+}
+
 async function main() {
+
     //get the list of the songs
     let songs = await getSongs();
-    console.log(songs);
+    // console.log(songs);
+
 
     let songUl = document.querySelector('.songList').getElementsByTagName('ul')[0];
+
+    //show all the songs in the playlist
     for (const song of songs) {
-        songUl.innerHTML = songUl.innerHTML + `<li>${song.replaceAll("%20", " ")}</li>`;
+        songUl.innerHTML = songUl.innerHTML + `
+                            <li>
+                                <img class="invert" src="/img/music.svg" alt="">
+                                <div class="info">
+                                    <div>${song.replaceAll("%20", " ")}</div>
+                                    <div>Satyam</div>
+                                </div>
+                                <div class="playNow">
+                                    <span>Play Now</span>
+                                    <img class="invert" src="/img/playNow.svg" alt="">
+                                </div>
+                            </li>`;
     }
 
-    //play the first song
-    var audio = new Audio(songs[0]);
-    audio.play();
+    //Attach the event listeners to each songs
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", (element) => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML)
+        })
+    })
 
-    audio.addEventListener("loadeddata", () => {
-        let duration = audio.duration;
-        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    // Attach the event listeners to the play, next, and previous
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play();
+            play.src = "/img/pause.svg";
+        }
+        else {
+            currentSong.pause();
+            play.src = "/img/play.svg";
+        }
+    })
+
+    // Listen for timeupdate event
+    currentSong.addEventListener("timeupdate", () => {
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`;
     })
 }
 
